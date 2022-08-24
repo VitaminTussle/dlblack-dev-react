8082c2e9fb5efe7eb86c12764043096e69ebe118
Deuce Black <deuce2black@gmail.com>
2022-08-23 23:59:35 -0500

    migrated to preferred file structure

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className='flex flex-row justify-between bg-slate-700 text-white p-4'>
-            <div className='flex'>
-                logo here
+        <div className='flex flex-row justify-between items-center bg-slate-700 text-white p-4'>
+            <div className='flex h-16'>
+                <img src={require('../assets/logo.png')} />
             </div>
-            <div className='flex flex-row justify-between'>
-                <div className='flex'>
+            <div className='flex flex-row justify-between text-xl'>
+                <div className='flex mr-8'>
                     Projects
                 </div>
-                <div className='flex'>
+                <div className='flex mr-8'>
                     Pockets
                 </div>
                 <div className='flex'>
